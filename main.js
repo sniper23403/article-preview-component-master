e05079a2dbdcba3b53f5ce4d3555bf3167298d9e
sniper23403 <100159541+sniper23403@users.noreply.github.com>
2024-04-20 23:40:50 +0100

    update

---
+++ main.js
@@ -0,0 +1,5 @@
+function toggleShare() {
+  const shareButton = document.querySelector('.share-btn');
+  const shareBar = document.querySelector('.share-bar');
+  shareBar.classList.toggle('active');
+}
